bf7bf6ecbf5851b73327f96486b65d16c8dd5988
SakibAhamedKhan <Sakibahamedkhan01@gmail.com>
2024-11-16 03:19:06 +0600

    Service advertis section done

--- src/components/carousels/HomeCarousel.tsx
+++ src/components/carousels/HomeCarousel.tsx
@@ -16,7 +16,7 @@ export function HomeCarousel() {
       <CarouselContent>
         {Array.from({ length: 5 }).map((_, index) => (
           <CarouselItem key={index} className="w-full">
-            <Card className="w-full  h-[400px] md:h-[600px] overflow-hidden rounded-none relative border-none">
+            <Card className="w-full  h-[400px] md:h-[500px] overflow-hidden rounded-none relative border-none">
               <CardContent className="p-0 h-full">
                 <img
                   src={`https://www.wework.com/ideas/wp-content/uploads/sites/4/2021/08/20201008-199WaterSt-2_v1-scaled.jpg`}
@@ -24,14 +24,14 @@ export function HomeCarousel() {
                   className="w-full h-full object-cover"
                 />
                 {/* Dark Overlay */}
-                <div className="absolute inset-0 bg-black opacity-50"></div>
+                <div className="absolute inset-0 bg-black opacity-60"></div>
                 {/* Centered Text and Button */}
                 <div className="absolute inset-0 flex items-center justify-center text-center text-white z-10">
                   <div className="p-16">
                     <h1 className="text-3xl md:text-5xl lg:text-6xl font-semibold mb-4 text-white">
                       Find your next Meeting
                     </h1>
-                    <p className="text-gray-200">
+                    <p className="text-gray-200 text-xl mt-5">
                       Search deals on Meeting Rooms and much more...
                     </p>
                     <Button
@@ -53,6 +53,7 @@ export function HomeCarousel() {
       <CarouselNext className="absolute right-4 top-1/2 -translate-y-1/2 z-10 bg-white text-black p-5 rounded-full">
         →
       </CarouselNext>
+      <div className="h-[10px] w-full bg-[#44FD60]"></div>
     </Carousel>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,33 @@
 import { HomeCarousel } from '@/components/carousels/HomeCarousel'
 import React from 'react'
+import { MdOutlineEventAvailable,MdAssuredWorkload  } from "react-icons/md";
+import { RiCalendarScheduleLine } from "react-icons/ri";
+import { BiSupport } from "react-icons/bi";
+
 
 const Home = () => {
   return (
     <div>
-      <div className='flex justify-center items-center'>
+      <div className=''>
         <HomeCarousel/>
+        <div className='bg-[#002F76] p-4 md:p-14 grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 text-white'>
+          <div className='flex flex-col justify-center items-center'>
+            <MdOutlineEventAvailable className='text-3xl md:text-6xl'/>
+            <h1 className='text-sm md:text-2xl font-semibold mt-5 text-center'>Real-Time Availability</h1>            
+          </div>
+          <div className='flex flex-col justify-center items-center'>
+            <MdAssuredWorkload className='text-3xl md:text-6xl'/>
+            <h1 className='text-sm md:text-2xl font-semibold mt-5 text-center'>Instant Booking Confirmation</h1>            
+          </div>
+          <div className='flex flex-col justify-center items-center'>
+            <RiCalendarScheduleLine className='text-3xl md:text-6xl'/>
+            <h1 className='text-sm md:text-2xl font-semibold mt-5 text-center'>Flexible Scheduling</h1>            
+          </div>
+          <div className='flex flex-col justify-center items-center'>
+            <BiSupport className='text-3xl md:text-6xl'/>
+            <h1 className='text-sm md:text-2xl font-semibold mt-5 text-center'> 24/7 Support</h1>            
+          </div>
+        </div>
       </div>
 
     </div>
