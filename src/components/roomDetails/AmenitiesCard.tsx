3218f7386c4f153c8a2361aa77dfdabd033ab549
unknown <sakibahamedkhan01@gmail.com>
2024-11-30 18:14:47 +0600

    Room details work on 50% done

---
+++ src/components/roomDetails/AmenitiesCard.tsx
@@ -0,0 +1,140 @@
+import React from "react";
+import {
+  FaWifi,
+  FaProjectDiagram,
+  FaPencilAlt,
+  FaPhoneAlt,
+  FaTv,
+  FaTemperatureHigh,
+  FaClipboardList,
+  FaVideo,
+  FaBolt,
+  FaUsers,
+  FaTable,
+  FaVolumeUp,
+  FaPlug,
+  FaCoffee,
+  FaPrint,
+  FaSun,
+  FaAdjust,
+  FaThLarge,
+  FaBuilding,
+  FaCar,
+  FaLock,
+  FaMagic,
+  FaBath,
+} from "react-icons/fa";
+import { FaUserGroup } from "react-icons/fa6";
+const amenitiesList = [
+  { value: "wifi", label: "Wi-Fi", icon: <FaWifi className="text-3xl text-gray-600" /> },
+  {
+    value: "projector",
+    label: "Projector/Screen",
+    icon: <FaProjectDiagram className="text-3xl text-gray-600" />,
+  },
+  {
+    value: "whiteboard",
+    label: "Whiteboard/Markers",
+    icon: <FaPencilAlt className="text-3xl text-gray-600" />,
+  },
+  {
+    value: "conferencePhone",
+    label: "Conference Phone",
+    icon: <FaPhoneAlt className="text-3xl text-gray-600" />,
+  },
+  { value: "tv", label: "TV or Monitor", icon: <FaTv className="text-3xl text-gray-600" /> },
+  {
+    value: "ac",
+    label: "Air Conditioning/Heating",
+    icon: <FaTemperatureHigh className="text-3xl text-gray-600" />,
+  },
+  {
+    value: "flipcharts",
+    label: "Flipcharts",
+    icon: <FaClipboardList className="text-3xl text-gray-600" />,
+  },
+  {
+    value: "videoConferencing",
+    label: "Video Conferencing Equipment",
+    icon: <FaVideo className="text-3xl text-gray-600" />,
+  },
+  {
+    value: "chargingStations",
+    label: "Charging Stations",
+    icon: <FaBolt className="text-3xl text-gray-600" />,
+  },
+  { value: "seating", label: "Seating", icon: <FaUsers className="text-3xl text-gray-600" /> },
+  { value: "tables", label: "Tables", icon: <FaTable className="text-3xl text-gray-600" /> },
+  {
+    value: "audioSystem",
+    label: "Audio System",
+    icon: <FaVolumeUp className="text-3xl text-gray-600" />,
+  },
+  {
+    value: "powerOutlets",
+    label: "Power Outlets",
+    icon: <FaPlug className="text-3xl text-gray-600" />,
+  },
+  {
+    value: "coffeeStation",
+    label: "Coffee/Water Station",
+    icon: <FaCoffee className="text-3xl text-gray-600" />,
+  },
+  {
+    value: "printerScanner",
+    label: "Printer/Scanner",
+    icon: <FaPrint className="text-3xl text-gray-600" />,
+  },
+  {
+    value: "naturalLighting",
+    label: "Natural Lighting",
+    icon: <FaSun className="text-3xl text-gray-600" />,
+  },
+  {
+    value: "curtains",
+    label: "Curtains/Blinds",
+    icon: <FaAdjust className="text-3xl text-gray-600" />,
+  },
+  {
+    value: "videoWalls",
+    label: "Video Walls",
+    icon: <FaThLarge className="text-3xl text-gray-600" />,
+  },
+  {
+    value: "phoneBooth",
+    label: "Phone Booth",
+    icon: <FaBuilding className="text-3xl text-gray-600" />,
+  },
+  { value: "parking", label: "Parking", icon: <FaCar className="text-3xl text-gray-600" /> },
+  {
+    value: "securityAccess",
+    label: "Security Access",
+    icon: <FaLock className="text-3xl text-gray-600" />,
+  },
+  {
+    value: "receptionArea",
+    label: "Reception Area",
+    icon: <FaUserGroup className="text-3xl text-gray-600" />,
+  },
+  {
+    value: "cleaningServices",
+    label: "Cleaning Services",
+    icon: <FaMagic className="text-3xl text-gray-600" />,
+  },
+  {
+    value: "restRooms",
+    label: "Rest Rooms",
+    icon: <FaBath className="text-3xl text-gray-600" />,
+  },
+];
+const AmenitiesCard = ({amenities}:any) => {
+  console.log(amenities);
+  const findData = amenitiesList.find(item=> item.value===amenities);
+  console.log(findData);
+  return <div className="flex justify-center items-center border w-fit p-4 rounded-lg gap-4 hover:bg-gray-200 cursor-default">
+    <div>{findData?.icon}</div>
+    <div className="text-md">{findData?.label}</div>
+  </div>;
+};
+
+export default AmenitiesCard;
--- src/pages/RoomDetails.tsx
+++ src/pages/RoomDetails.tsx
@@ -1,8 +1,9 @@
 import { useGetSingleRoomQuery } from "@/redux/features/customer/roomApi.api";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { FaLocationDot } from "react-icons/fa6";
-import { Image } from "antd";
+import { Button, Image } from "antd";
+import ReactMarkdown from "react-markdown";
 import {
   Carousel,
   CarouselContent,
@@ -10,19 +11,19 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import AmenitiesCard from "@/components/roomDetails/AmenitiesCard";
 
 const RoomDetails = () => {
   const { id } = useParams();
   const { data, isFetching } = useGetSingleRoomQuery(id);
-  let roomData;
-  if (!isFetching) {
-    roomData = data?.data;
-  }
-  console.log(roomData);
+
+  const roomData = data?.data;
+
   return (
     <div>
       <div className="mx-auto max-w-7xl px-4 md:px-6 lg:px-8 my-8 md:my-12">
         <div className="grid grid-cols-7 gap-4">
+          {/* Title and image */}
           <div className="col-span-5 border">
             <h1 className="text-2xl font-bold">{roomData?.name}</h1>
             <div className="flex items-center gap-1 my-2">
@@ -41,17 +42,17 @@ const RoomDetails = () => {
               <Image
                 className="h-auto max-w-full rounded-lg"
                 src={`${roomData?.thumbnail[0]?.url}`}
-                alt=""
+                alt="Room Thumbnail"
               />
             </div>
             <Carousel className="my-2">
               <CarouselContent className="ml-[1px]">
-                {roomData?.extraImages.map((item:any) => (
-                  <div className="mr-4">
+                {roomData?.extraImages.map((item: any) => (
+                  <div className="mr-4" key={item.public_id}>
                     <Image
                       className="h-auto rounded-lg !w-[80px] md:!w-[200px]"
                       src={`${item.url}`}
-                      alt=""
+                      alt="Extra Room Image"
                     />
                   </div>
                 ))}
@@ -64,11 +65,39 @@ const RoomDetails = () => {
               </CarouselNext>
             </Carousel>
           </div>
-          <div className="border">hi</div>
+          {/* Review and map */}
+          <div className="col-span-2 border">hi</div>
         </div>
-        
+
+        {/* amenities */}
+        <div className="flex gap-4 my-8 flex-wrap">
+          {roomData?.amenities.map((item: any, index: number) => (
+            <AmenitiesCard amenities={item} key={index} />
+          ))}
+        </div>
+        {/* description and Reserve */}
+        <div className="grid grid-cols-7 gap-4">
+          <div className="col-span-5">
+            <p className="leading-8 text-justify text-lg">
+              {roomData?.description}
+            </p>
+          </div>
+          <div className="col-span-2 ">
+            <div className="border p-4 rounded-lg">
+              <p>
+                <span className="font-medium">Price Per Slot:</span> $
+                {roomData?.pricePerSlot}
+              </p>
+              <p className="text-sm text-gray-600 mt-3 mb-2">
+                * We given money back generate
+              </p>
+              <Button className="bg-[#002F76] text-white w-full h-[40px] text-md md:text-lg font-semibold mb-2">
+                Reserve
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
-      
     </div>
   );
 };
